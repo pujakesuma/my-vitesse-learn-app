fa31e2da76924aacd9619b4dd0673bf5f03243f1
Ahmad Ridwan <ahmadridwan.3107@gmail.com>
2022-03-12 22:14:56 +0700

    feat: add detail class page

--- src/pages/class/[name].vue
+++ src/pages/class/[name].vue
@@ -1,25 +1,51 @@
 <script setup lang="ts">
+import { useClassStore } from '~/store/class'
 const props = defineProps<{ name: string }>()
 const router = useRouter()
+const store = useClassStore()
+
+const idClass = parseInt(props.name)
+const isFetching = computed(() => store.isFetching)
+const detail = computed(() => store.getDetailClass)
+
+// mounted
+onMounted(() => {
+  store.fetchDetail(idClass)
+})
 </script>
 
 <template>
-  <div>
-    <div i-carbon-pedestrian text-4xl inline-block />
-    <p>
-      Hi, {{ props.name }}
-    </p>
-    <p text-sm op50>
-      <em>Dynamic route!</em>
-    </p>
-
+  <div h-full flex justify-center pt-4rem>
+    <div v-if="isFetching" class="flex flex-col sm:flex-col md:flex-row space-x-0 sm:space-x-0 md:space-x-4">
+      <div class="placeholder mb-4 sm:mb-4 md:mb-0 w-18rem sm:w-18rem md:w-lg">
+        <div rounded-xl h-6 bg-gray-400 mb-4 class="w-1/4" />
+        <div v-for="idx in 3" :key="idx" rounded-xl h-3 bg-gray-400 mb-2 class="w-3/5" />
+        <div class="w-1/4" h-4 bg-gray-400 mt-4 rounded-xl />
+      </div>
+    </div>
     <div>
-      <button
-        class="btn m-3 text-sm mt-8"
-        @click="router.back()"
-      >
-        Back
-      </button>
+      <h2 class="text-3xl font-bold">
+        {{ detail.name }}
+      </h2>
+      <p>
+        {{ detail.description }}
+      </p>
+      <div v-for="(mentor, i) in detail.mentors" :key="i">
+        <div>
+          {{ mentor.name }}
+        </div>
+        <p>
+          {{ mentor.description }}
+        </p>
+      </div>
+      <div>
+        <button
+          class="btn m-3 text-sm mt-8"
+          @click="router.back()"
+        >
+          Back
+        </button>
+      </div>
     </div>
   </div>
 </template>
