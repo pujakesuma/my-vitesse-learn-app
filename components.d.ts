d1a22689f2e5fbff3860045e45d294ecc4b8b475
Ahmad Ridwan <ahmadridwan.3107@gmail.com>
2022-03-13 13:45:36 +0700

    auto import component

--- components.d.ts
+++ components.d.ts
@@ -4,7 +4,7 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    Counter: typeof import('./src/components/Counter.vue')['default']
+    Card: typeof import('./src/components/Card.vue')['default']
     Footer: typeof import('./src/components/Footer.vue')['default']
     Modal: typeof import('./src/components/Modal.vue')['default']
     Register: typeof import('./src/components/Register.vue')['default']
