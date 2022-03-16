2dc94774d64c8790fcbbefe62bc1b65ed04a1a8b
Ahmad Ridwan <ahmadridwan.3107@gmail.com>
2022-03-16 19:00:33 +0700

    add component test

--- test/component.test.ts
+++ test/component.test.ts
@@ -1,22 +1,17 @@
 import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
-import Counter from '../src/components/Counter.vue'
+import Card from '../src/components/Card.vue'
 
-describe('Counter.vue', () => {
+describe('Card.vue', () => {
   it('should render', () => {
-    const wrapper = mount(Counter, { props: { initial: 10 } })
-    expect(wrapper.text()).toContain('10')
+    const wrapper = mount(Card, { props: { customClass: 'w-full lg:w-3/5' } })
+    expect(wrapper.text()).toContain('')
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should be interactive', async() => {
-    const wrapper = mount(Counter, { props: { initial: 0 } })
-    expect(wrapper.text()).toContain('0')
-
-    expect(wrapper.find('.inc').exists()).toBe(true)
-
-    await wrapper.get('button').trigger('click')
-
-    expect(wrapper.text()).toContain('1')
+    const wrapper = mount(Card, { props: { customClass: '' } })
+    expect(wrapper.text()).toContain('')
+    expect(wrapper.text()).toContain('')
   })
 })
